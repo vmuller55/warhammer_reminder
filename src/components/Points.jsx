6b45a3db1af415b62dde37c4397678d3496bb8a3
Vincent Muller <v.muller55000@gmail.com>
2023-02-01 20:05:36 +0100

    add some json unit start select unit page

--- src/components/Points.jsx
+++ src/components/Points.jsx
@@ -1,13 +1,18 @@
 import "../assets/styles/components/points.scss"
 
 import React from 'react'
-import RangeSlider from "./Slider"
+import { useState } from "react"
 
-function Points() {
+
+function Points({ props }) {
+
+    const [currentValue, setCurrentValue] = useState('')
+    props = currentValue
     return (
         <div className="pointBox">
             <h4>Combien de points?</h4>
-            <RangeSlider/>
+            <input id="rangePoint" type={"range"} min={"500"} max={"2000"} step={"100"} value={currentValue} onChange={e => setCurrentValue(e.target.value)}></input>
+            <h5>{currentValue}</h5>
         </div>
     )
 }
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,12 +1,34 @@
-import Points from "../components/Points"
+import "../assets/styles/components/points.scss"
+import React from "react"
+import { useState } from "react"
+import data from "../assets/data/stormcast.json"
+
 
 function Order () {
-    return (
+    let [currentValue, setCurrentValue] = useState('')
+    let [isSelect, setIsSelect] = useState(false)
+    return ( 
         <div className>
-            <div className="points">
-                <Points/>
+            <div className="pointBox">
+                <h4>Combien de points?</h4>
+                <input id="rangePoint" type={"range"} min={"0"} max={"3000"} step={"100"}  value={currentValue} onChange={e => setCurrentValue(e.target.value)}></input>
+                <h5>{currentValue? currentValue : 1500}</h5>
             </div>
-            <div className="addUnit"></div>
+            <div className="wrapper">
+                <div className="addUnit">
+                    <h4>Ajouter une unité</h4>
+                    <ul>
+                        {data.map((unit) => {
+                            return (
+                                <li onClick={() => setIsSelect(true)}><p>{unit.name}</p></li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className={`unitAdded${isSelect?"-active":"-inactive"}`}>
+                    <p>Je suis ici</p>
+                </div>
+            </div>
         </div>
     )
 
